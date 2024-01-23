4d2c3bed459bba2440e143102caf1d48b7317c5f
Teika101 <ragoska@gmail.com>
2024-01-23 21:41:28 +0000

    Added Mansion, Made so ever player in lobby Switches to mansion scene

--- WhoDealtIt/Assets/Scripts/GameManager.cs
+++ WhoDealtIt/Assets/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -13,7 +14,9 @@ public class GameManager : MonoBehaviour
     public GameObject SceneCamera;
     public TMP_Text PingText;
     public GameObject PauseUI;
+    public GameObject DebugUI;
     private bool Off = false;
+    private bool DBOff = false;
 
     public GameObject PlayerFeed;
     public GameObject FeedGrid;
@@ -27,7 +30,11 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
-        if(GameCanvas != null)
+
+        PhotonNetwork.automaticallySyncScene = true;
+        SpawnPlayerMansion();
+
+        if (GameCanvas != null)
         {
             GameCanvas.SetActive(true);
         }
@@ -38,18 +45,18 @@ public class GameManager : MonoBehaviour
     private void Update()
     {
         CheckInput();
-        if(Ping != null)
+        if (Ping != null)
         {
             PingText.text = "PING:" + PhotonNetwork.GetPing();
         }
-        
+
 
     }
 
     //Open PauseUI
     private void CheckInput()
     {
-        if(Off && Input.GetKeyDown(KeyCode.Escape))
+        if (Off && Input.GetKeyDown(KeyCode.Escape))
         {
             PauseUI.SetActive(false);
             Off = false;
@@ -64,15 +71,31 @@ public class GameManager : MonoBehaviour
     // Start Button to Spawn Player
     public void SpawnPlayer()
     {
-        float randomValue = Random.Range(-1f,1f);
+        float randomValue = Random.Range(-1f, 1f);
 
         PhotonNetwork.Instantiate(PlayerPrefab.name, new Vector2(this.transform.position.x * randomValue, this.transform.position.y), Quaternion.identity, 0);
         GameCanvas.SetActive(false);
         SceneCamera.SetActive(false);
 
-        if(ChangedPingOption)
+        if (ChangedPingOption)
         {
             SettingsUI.GetComponentInChildren<Toggle>().isOn = true;
+        }
+    }
+
+    public void SpawnPlayerMansion()
+    {
+        if (SceneManager.GetActiveScene().name == "Mansion")
+        {
+            float randomValue = Random.Range(-1f, 1f);
+
+            PhotonNetwork.Instantiate(PlayerPrefab.name, new Vector2(this.transform.position.x * randomValue, this.transform.position.y), Quaternion.identity, 0);
+            SceneCamera.SetActive(false);
+
+            if (ChangedPingOption)
+            {
+                SettingsUI.GetComponentInChildren<Toggle>().isOn = true;
+            }
         }
     }
 
@@ -103,7 +126,7 @@ public class GameManager : MonoBehaviour
         {
             Ping.SetActive(false);
             PingOn = false;
-        }  
+        }
         else
         {
             Ping.SetActive(true);
@@ -117,7 +140,7 @@ public class GameManager : MonoBehaviour
 
     private void OnPhotonPlayerConnected(PhotonPlayer player)
     {
-        GameObject obj = Instantiate(PlayerFeed, new Vector2(0, 0), Quaternion.identity); 
+        GameObject obj = Instantiate(PlayerFeed, new Vector2(0, 0), Quaternion.identity);
         obj.transform.SetParent(FeedGrid.transform, false);
         obj.GetComponent<TMP_Text>().text = player.name + " joined the game";
         obj.GetComponent<TMP_Text>().color = Color.green;
@@ -130,5 +153,38 @@ public class GameManager : MonoBehaviour
         obj.GetComponent<TMP_Text>().text = player.name + " left the game";
         obj.GetComponent<TMP_Text>().color = Color.red;
     }
+
+    public void MansionLVL()
+    {
+        PhotonNetwork.LoadLevel("Mansion");
+    }
+
+    private void OpenDebug()
+    {
+        if (DBOff && Input.GetKeyDown(KeyCode.F1))
+        {
+            DebugUI.SetActive(false);
+            DBOff = false;
+        }
+        else if (!DBOff && Input.GetKeyDown(KeyCode.F1))
+        {
+            DebugUI.SetActive(true);
+            DBOff = true;
+        }
+    }
+
+
+    /*For Future Refrence 
+     
+    public void OnClick_StartGame()
+    {
+         if (PhotonNetwork.IsMasterClient)
+      {
+         PhotonNetwork.CurrentRoom.IsOpen = false;
+         PhotonNetwork.CurrentRoom.IsVisible = false;
+         PhotonNetwork.LoadLevel("Mansion")
+      }
+    }
+    */
 }
 
