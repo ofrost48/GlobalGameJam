27cb561105c2616393eb1befb9ae4b25f82c6b0c
ofrost48 <117731487+ofrost48@users.noreply.github.com>
2024-01-23 23:40:52 +0000

    Added Map system with blur

--- WhoDealtIt/Assets/Scripts/MapOpener.cs
+++ WhoDealtIt/Assets/Scripts/MapOpener.cs
@@ -1,30 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection.Emit;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.Rendering.PostProcessing;
 
 public class MapOpener : MonoBehaviour
 {
+    public GameObject Map;
+    public PostProcessVolume blur;
+    public GameObject playerCam;
 
-    private Inputs input = null;
-    private bool isOpen = false;
+    private bool isActive;
 
     private void Awake()
     {
-        input = new Inputs();
-        input.MapAction.OpenMap.performed += x => MapPressed();
-        input.MapAction.CloseMap.performed += x => MapReleased();
+        Map.SetActive(false);
+        isActive = false;
+        blur.enabled = false;
     }
 
-    private void MapPressed()
+    private void Update()
     {
-        isOpen = true;
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+            blur.enabled = !blur.enabled;
+
+            if (isActive == false)
+            {
+                Map.SetActive(true);
+                isActive = true;
+            }
+            else if (isActive == true)
+            {
+                Map.SetActive(false);
+                isActive = false;
+            }
+
+        }
     }
 
-    private void MapReleased()
-    {
-        isOpen = false;
-    }
 
 
 }
